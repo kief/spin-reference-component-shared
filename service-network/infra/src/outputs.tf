d8b1ebd64858464a21bf2aa2eccaf47a8aca6db3
kief <kmorris@thoughtworks.com>
2018-03-27 19:25:15 +0100

    Export configuration to ssm parameter store for other services to use

--- service-network/infra/src/outputs.tf
+++ service-network/infra/src/outputs.tf
@@ -3,3 +3,21 @@ output "bastion_public_dns" {
   description = "The public DNS name to reach the bastion."
   value = "${module.bastion_load_balancer.dns_name}"
 }
+
+resource "aws_ssm_parameter" "bastion_public_dns" {
+  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/bastion_public_dns"
+  type  = "String"
+  value = "${module.bastion_load_balancer.dns_name}"
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/private_subnet_ids"
+  type  = "StringList"
+  value = "${module.base-network.private_subnet_ids}"
+}
+
+resource "aws_ssm_parameter" "allow_ssh_from_bastion_security_group_id" {
+  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/allow_ssh_from_bastion_security_group_id"
+  type  = "String"
+  value = "${module.bastion.allow_ssh_from_bastion_security_group_id}"
+}
