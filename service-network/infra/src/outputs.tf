54b775a94c8f8c9e5a958d95efe57c9cdf868c1e
kief <kmorris@thoughtworks.com>
2018-03-29 15:21:59 +0100

    Push the bastion ssh key into a parameter, so other services can grab it.

--- service-network/infra/src/outputs.tf
+++ service-network/infra/src/outputs.tf
@@ -5,19 +5,22 @@ output "bastion_public_dns" {
 }
 
 resource "aws_ssm_parameter" "bastion_public_dns" {
-  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/bastion_public_dns"
-  type  = "String"
-  value = "${module.bastion_load_balancer.dns_name}"
+  name      = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/bastion_public_dns"
+  type      = "String"
+  value     = "${module.bastion_load_balancer.dns_name}"
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/private_subnet_ids"
-  type  = "StringList"
-  value = "${module.base-network.private_subnet_ids}"
+  name      = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/private_subnet_ids"
+  type      = "StringList"
+  value     = "${module.base-network.private_subnet_ids}"
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "allow_ssh_from_bastion_security_group_id" {
-  name  = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/allow_ssh_from_bastion_security_group_id"
-  type  = "String"
-  value = "${module.bastion.allow_ssh_from_bastion_security_group_id}"
+  name      = "/${var.estate_id}/${var.component}/${var.service}/${var.deployment_id}/allow_ssh_from_bastion_security_group_id"
+  type      = "String"
+  value     = "${module.bastion.allow_ssh_from_bastion_security_group_id}"
+  overwrite = true
 }
